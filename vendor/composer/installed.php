685202d2ecb6c880f5a1ca3c3360c29b38d26764
Akshay Prabhakaran <akshay.prabhakaran@qburst.com>
2024-03-21 12:16:22 +0530

    added auto discovery

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'imakshaypm/contact',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '358599717508a40fed5491fb367b5d8687ec90a8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'imakshaypm/contact' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '358599717508a40fed5491fb367b5d8687ec90a8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
